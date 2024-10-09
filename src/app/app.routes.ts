26ee4ddbc0367686075674996424e1c0466ed9c6
Baptiste Dupache <baptistedupache@gmail.com>
2024-10-09 13:44:11 +0200

    🔨 page accueil dans l'espace commerce fini

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,19 @@ import { AuthGuard } from './auth/auth.guard';
 import { AdminComponent } from './pages/admin/admin.component';
 import { AccueilCommerceComponent } from './pages/accueil-commerce/accueil-commerce.component';
 import { AccueilGlobalComponent } from './pages/accueilGlobal/accueil.component';
+import { ProfilCommercantComponent } from './pages/profil-commercant/profil-commercant.component';
+import { GestionDesProduitsComponent } from './pages/gestion-des-produits/gestion-des-produits.component';
+import { GestionDesCommandesComponent } from './pages/gestion-des-commandes/gestion-des-commandes.component';
 
 export const routes: Routes = 
 [
     { path: 'login', component: LoginFormComponent },
     { path: 'register', component: RegisterFormComponent },
     { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] },
+    { path: 'accueilGlobal', component:AccueilGlobalComponent },
+
     { path: 'commerce/accueil', component:AccueilCommerceComponent },
-    { path: 'accueilGlobal', component:AccueilGlobalComponent }
+    { path: 'commerce/profil-commerçant', component:ProfilCommercantComponent },
+    { path: 'commerce/gestion-des-produits', component:GestionDesProduitsComponent },
+    { path: 'commerce/gestion-des-commandes', component:GestionDesCommandesComponent },
 ];
