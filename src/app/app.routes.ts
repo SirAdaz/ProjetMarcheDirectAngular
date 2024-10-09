792986bafb650921d92edd1719cd20c9d2fa0d0e
Baptiste Dupache <baptistedupache@gmail.com>
2024-10-09 11:11:27 +0200

    🚧 création de la page accueil + ✨  ajout de la route commerce/accueil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginFormComponent } from './pages/login-form/login-form.component';
+import { AccueilCommerceComponent } from './pages/accueil-commerce/accueil-commerce.component';
 
 export const routes: Routes = 
 [
     {path: 'login', component:LoginFormComponent},
+    {path: 'commerce/accueil', component:AccueilCommerceComponent},
 ];
