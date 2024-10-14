2be282f1687624fe9d84aa0fb079e35c720abbbd
elyayusd <133009649+elyayus@users.noreply.github.com>
2024-10-14 14:40:00 +0200

    ✨ Ajout Page Avis de l'utilisateur

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
-import { LoginFormComponent } from './pages/login-form/login-form.component';
-import { RegisterFormComponent } from './pages/register-form/register-form.component';
 import { AuthGuard } from './auth/auth.guard';
-import { AdminComponent } from './pages/admin/admin.component';
 import { AccueilCommerceComponent } from './pages/accueil-commerce/accueil-commerce.component';
 import { AccueilGlobalComponent } from './pages/accueilGlobal/accueil.component';
-import { UserProfilComponent } from './pages/user-profil/user-profil.component';
-import { UserCommandHistoryComponent } from './pages/user-command-history/user-command-history.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { LoginFormComponent } from './pages/login-form/login-form.component';
+import { RegisterFormComponent } from './pages/register-form/register-form.component';
 import { UserCartComponent } from './pages/user-cart/user-cart.component';
+import { UserCommandHistoryComponent } from './pages/user-command-history/user-command-history.component';
+import { UserCommentComponent } from './pages/user-comment/user-comment.component';
+import { UserProfilComponent } from './pages/user-profil/user-profil.component';
 
 export const routes: Routes = 
 [
@@ -21,4 +22,5 @@ export const routes: Routes =
     { path: 'user-profil', component: UserProfilComponent},
     { path: 'user-command-history/:userId', component: UserCommandHistoryComponent },
     { path: 'user-cart', component: UserCartComponent },
+    { path: 'user-comments', component: UserCommentComponent },
 ];
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,15 +1,15 @@
 // Service pour le User
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { User } from '../models/user.model'; 
 import { command } from '../models/command.model';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl = 'http://localhost:8000/api/users'; 
+  private apiUrl = 'http://localhost:8000/api/users';
 
   constructor(private http: HttpClient) { }
 
