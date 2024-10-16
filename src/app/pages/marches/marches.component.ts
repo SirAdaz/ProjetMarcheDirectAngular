03c650aa03f6567ef419f21317dd0265b3beefe8
Adaz SirAdaz <mrxptdrgame@gmail.com>
2024-10-16 10:03:10 +0200

    💄✨ Mise en place de la disparition des boutons Précédent et Suivant lorsqu'ils ne sont pas disponibles

--- src/app/pages/marches/marches.component.ts
+++ src/app/pages/marches/marches.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+
+import { Component, AfterViewInit } from '@angular/core';
 import Marche from '../../models/marche.model';
 import { MarchesService } from '../../services/marches.service';
 import { RouterLink } from '@angular/router';
@@ -10,32 +11,57 @@ import { RouterLink } from '@angular/router';
   templateUrl: './marches.component.html',
   styleUrl: './marches.component.css'
 })
-export class MarchesComponent 
-{
-  marches!: Marche[]
-  i=1;
+export class MarchesComponent implements AfterViewInit {
+  marches!: Marche[];
+  i = 1;
   urlImg = "https://127.0.0.1:8000/images/";
+  btnPrecedent!: HTMLElement | null;
+  btnSuivant!: HTMLElement | null;
 
   constructor(private marchesServices: MarchesService) {}
 
-  public getMarchesOtherPagesMoins(): void { // Méthode pour récupérer les autres pages des books
-    this.i = this.i-1;
-    if (this.i < 1) {
-      this.i = 1;
+  ngAfterViewInit(): void {
+    this.btnPrecedent = document.getElementById("btn-precedent");
+    this.btnSuivant = document.getElementById("btn-suivant");
+    this.updateButtonVisibility();
+  }
+
+  public getMarchesOtherPagesMoins(): void {
+    if (this.i > 1) {
+      this.i = this.i - 1;
+      this.marchesServices.getMarchesOtherPages(this.i).subscribe((data) => {
+        this.marches = data;
+        this.updateButtonVisibility();
+      });
     }
-    this.marchesServices.getMarchesOtherPages(this.i).subscribe((data) => { this.marches = data})
   }
   
-  public getMarchesOtherPagesPlus(): void { // Méthode pour récupérer les autres pages des books
+  public getMarchesOtherPagesPlus(): void {
     this.marchesServices.getMarchesOtherPages(this.i + 1).subscribe((data) => {
-      if (data && data.length > 0) { // Vérifie s'il y a des données sur la page suivante
+      if (data && data.length > 0) {
         this.i = this.i + 1;
         this.marches = data;
+        this.updateButtonVisibility();
       }
     });
   }
-
+  
   ngOnInit(): void {
-    this.marchesServices.getMarches().subscribe((data) => {this.marches = data});
+    this.marchesServices.getMarches().subscribe((data) => {
+      this.marches = data;
+      this.updateButtonVisibility();
+    });
+  }
+
+  private updateButtonVisibility(): void {
+    if (this.btnPrecedent) {
+      this.btnPrecedent!.style.display = this.i === 1 ? "none" : "block";
+    }
+
+    if (this.btnSuivant) {
+      this.marchesServices.getMarchesOtherPages(this.i + 1).subscribe((data) => {
+        this.btnSuivant!.style.display = data && data.length > 0 ? "block" : "none";
+      });
+    }
   }
 }
