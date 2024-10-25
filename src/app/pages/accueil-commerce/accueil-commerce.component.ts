d312f338f792433f0dd44b3547244c1223873451
elyayusd <133009649+elyayus@users.noreply.github.com>
2024-10-25 10:35:58 +0200

    Mise à jour de ma branche

---
+++ src/app/pages/accueil-commerce/accueil-commerce.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-accueil-commerce',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './accueil-commerce.component.html',
+  styleUrl: './accueil-commerce.component.css'
+})
+export class AccueilCommerceComponent {
+
+}
