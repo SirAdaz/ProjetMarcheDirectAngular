5d611b3f1ca6e549919ae1ad7132025b01383f18
Adaz SirAdaz <mrxptdrgame@gmail.com>
2024-10-25 12:43:08 +0200

    🔨 Mise à jour du panier pour gérer la mise à jour du stock

--- src/app/pages/pages-client/user-cart/user-cart.component.ts
+++ src/app/pages/pages-client/user-cart/user-cart.component.ts
@@ -8,6 +8,7 @@ import User from '../../../models/user.model';
 import { Observable } from 'rxjs';
 import Commande from '../../../models/command.model';
 import ProduitCommande from '../../../models/produitCommande.mode';
+import { ProduitsService } from '../../../services/produits.service';
 
 // Déclaration du composant UserCart
 @Component({
@@ -28,12 +29,15 @@ export class UserCartComponent implements OnInit {
     private userService: UserService, // Service pour les utilisateurs
     private authService: AuthService, // Service pour l'authentification
     private router: Router, // Service pour la navigation
-    private commandeService: CommandeService // Service pour les commandes
+    private commandeService: CommandeService, // Service pour les commandes
+    private productService: ProduitsService // Service pour les produits
   ) {}
 
   // Récupère les articles du panier stockés dans le localStorage
   getCartItems(): any[] {
-    return JSON.parse(localStorage.getItem('cart') || '[]');
+    const cart = JSON.parse(localStorage.getItem('cart') || '[]');
+    console.log('Cart items:', cart); // Log des articles dans le panier
+    return cart;
   }
 
   // Calcule le nombre total d'articles dans le panier
@@ -69,16 +73,16 @@ export class UserCartComponent implements OnInit {
 
   // Met à jour la quantité d'un article dans le panier
   updateCartItem(item: any, event: any): void {
-    const target = event.target as HTMLSelectElement; // Cible l'élément HTML du changement d'événement
-    const selectedQuantity = Number(target.value); // Récupère la quantité sélectionnée
-    const cart = this.getCartItems(); // Récupère les articles du panier
-    const index = cart.findIndex((ci: any) => ci.id === item.id); // Trouve l'index de l'article à mettre à jour
+    const selectedQuantity = Number((event.target as HTMLSelectElement).value);
+    const cart = this.getCartItems();
+    const index = cart.findIndex(ci => ci.id === item.id);
     if (index !== -1) {
-      cart[index].quantity = selectedQuantity; // Met à jour la quantité de l'article
-      this.cartItems = cart; // Met à jour la liste des articles du panier
-      localStorage.setItem('cart', JSON.stringify(cart)); // Enregistre le panier mis à jour
+        cart[index].quantity = selectedQuantity;
+        localStorage.setItem('cart', JSON.stringify(cart));
+        this.cartItems = [...cart]; // Met à jour l'affichage du panier
     }
-  }
+}
+
 
   // Calcule le prix total d'un commerçant spécifique
   getTotalPriceByMerchant(commercantId: string): string {
@@ -107,8 +111,6 @@ export class UserCartComponent implements OnInit {
     commercantIds.forEach((id) => {
       this.fetchCommercantInfo(id).subscribe((info) => {
         this.commercants.push(info); // Ajoute les informations du commerçant à la liste
-      }, error => {
-        // Gestion des erreurs lors de la récupération des informations du commerçant
       });
     });
   }
@@ -120,75 +122,113 @@ export class UserCartComponent implements OnInit {
 
   // Crée une nouvelle commande à partir des articles du panier
   newCommande(): void {
-    if (!this.authService.isLoggedIn()) { // Vérifie si l'utilisateur est connecté
-      this.router.navigate(['/login']); // Redirige vers la page de connexion si non connecté
-      return;
+    if (!this.authService.isLoggedIn()) {
+        this.router.navigate(['/login']);
+        return;
     }
 
-    const userId = this.authService.getUserId(); // Récupère l'ID de l'utilisateur
-    if (!userId) {
-      return; // Arrête l'exécution si l'ID utilisateur est introuvable
-    }
+    const userId = this.authService.getUserId();
+    if (!userId) return;
 
-    const cart = this.getCartItems(); // Récupère les articles du panier
-    const groupedItems = this.groupItemsByMerchant(cart); // Regroupe les articles par commerçant
-    const totalOrders = Object.keys(groupedItems).length; // Compte le nombre total de commandes
-    let successfulOrders = 0; // Compteur pour les commandes réussies
+    const cart = this.getCartItems();
+    const groupedItems = this.groupItemsByMerchant(cart);
+    const totalOrders = Object.keys(groupedItems).length;
+    let successfulOrders = 0;
 
     for (const [merchantId, items] of Object.entries(groupedItems)) {
-      this.fetchCommercantInfo(+merchantId).subscribe(merchantInfo => {
-        this.userService.getUserProfile(userId).subscribe(userInfo => {
-          const produits_commande: ProduitCommande[] = items.map(item => ({
-            productId: item.id,
-            quantity: item.quantity // Crée un tableau des produits commandés
-          }));
+        this.fetchCommercantInfo(+merchantId).subscribe(merchantInfo => {
+            this.userService.getUserProfile(userId).subscribe(userInfo => {
+                const produits_commande: ProduitCommande[] = items.map(item => ({
+                    productId: item.id,
+                    quantity: item.quantity
+                }));
 
-          const produits = items.map(item => ({
-            id: item.id,
-            productName: item.name,
-            description: item.description,
-            prix: item.price,
-            stock: item.stock,
-            imageFileName: item.imageFileName,
-            userProduct: item.user,
-            format: item.format // Crée un tableau des produits avec les détails
-          }));
+                // Vérifiez que le stock est suffisant avant de créer la commande
+                const insufficientStock = items.some(item => item.stock < item.quantity);
+                if (insufficientStock) {
+                    this.message = 'Quantité demandée supérieure au stock disponible.';
+                    return; // Sortir si le stock est insuffisant
+                }
+                
+                console.log('Items to process:', items); // Ajoutez ce log pour voir les données
+                const produits = items.map(item => ({
+                    id: item.id,
+                    productName: item.name,
+                    description: item.description,
+                    prix: item.price,
+                    stock: item.stock !== undefined ? item.stock : 0,
+                    imageFileName: item.imageFileName,
+                    userProduct: item.user,
+                    format: item.format
+                }));
 
-          const UserCommande = [
-            userInfo, // Informations de l'utilisateur client
-            merchantInfo // Informations du commerçant
-          ];
+                const UserCommande = [
+                    userInfo,
+                    merchantInfo
+                ];
 
-          const orderData: Commande = {
-            produits_commande: produits_commande,
-            etat: { id: 1, name: 'En attente' }, // État de la commande initial
-            UserCommande: UserCommande,
-            produits: produits // Informations sur les produits
-          };
+                const orderData: Commande = {
+                    produits_commande: produits_commande,
+                    etat: { id: 1, name: 'En attente' },
+                    UserCommande: UserCommande,
+                    produits: produits
+                };
 
-          // Appel à la méthode pour créer la commande
-          this.createOrder(orderData).subscribe(
-            response => {
-              successfulOrders++; // Incrémente le compteur de commandes réussies
-              this.message = 'Commande créée avec succès !'; // Message de réussite
-              
-              // Vérifie si toutes les commandes ont réussi
-              if (successfulOrders === totalOrders) {
-                this.clearCart(); // Vide le panier uniquement si toutes les commandes ont réussi
-              }
-            },
-            error => {
-              this.message = 'Erreur lors de la création de la commande. Veuillez réessayer.'; // Message d'échec
-            }
-          );
+                // Créez la commande
+                this.createOrder(orderData).subscribe(
+                    response => {
+                        successfulOrders++;
+                        this.message = 'Commande créée avec succès !';
+
+                        // Mettez à jour le stock
+                        // Mettez à jour le stock
+                        let updateStockPromises = items.map(item => {
+                          const stock = item.stock !== undefined ? item.stock : 0; // Valeur par défaut si undefined
+                          const quantity = item.quantity !== undefined ? item.quantity : 0; // Valeur par défaut si undefined
+                      
+                          // Ajoutez des logs pour vérifier les valeurs
+                          console.log(`Product ID: ${item.id}, Stock: ${stock}, Quantity requested: ${quantity}`);
+                      
+                          // Vérifiez si la quantité demandée est supérieure au stock
+                          if (quantity > stock) {
+                              this.message = `Quantité demandée pour ${item.productName} dépasse le stock disponible. Stock actuel: ${stock}.`;
+                              console.error(`Quantity requested exceeds available stock for product ID: ${item.id}.`);
+                              return Promise.reject(new Error(`Quantity requested exceeds available stock for product ID: ${item.id}.`));
+                          }
+                      
+                          const newStock = stock - quantity; // Calculer le nouveau stock
+                          console.log(`Updating stock for product ID: ${item.id}, New stock: ${newStock}`); // Log pour déboguer
+                          return this.productService.updateProductStock(item.id, newStock).toPromise();
+                      });
+                      
+
+                      
+
+                        Promise.all(updateStockPromises).then(() => {
+                            // Tout a réussi, videz le panier ici
+                            if (successfulOrders === totalOrders) {
+                                this.clearCart();
+                            }
+                        }).catch(error => {
+                            this.message = 'Erreur lors de la mise à jour du stock.';
+                            console.error('Erreur lors de la mise à jour du stock :', error);
+                        });
+                    },
+                    error => {
+                        this.message = 'Erreur lors de la création de la commande. Veuillez réessayer.';
+                    }
+                );
+            }, error => {
+                this.message = 'Erreur lors de la récupération des informations du client.';
+            });
         }, error => {
-          this.message = 'Erreur lors de la récupération des informations du client. Veuillez réessayer.'; // Message d'échec
+            this.message = 'Erreur lors de la récupération des informations du commerçant.';
         });
-      }, error => {
-        this.message = 'Erreur lors de la récupération des informations du commerçant. Veuillez réessayer.'; // Message d'échec
-      });
     }
-  }
+}
+
+
+
 
   // Regroupe les articles par commerçant
   groupItemsByMerchant(cart: any[]): { [key: string]: any[] } {
--- src/app/pages/produits/produits.component.ts
+++ src/app/pages/produits/produits.component.ts
@@ -1,197 +1,178 @@
-import { Component, OnInit, SimpleChanges } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProduitsService } from '../../services/produits.service';
 import Produit from '../../models/produit.model';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+// Déclaration du composant Produits
 @Component({
-  selector: 'app-produits',
-  standalone: true,
-  imports: [RouterLink, CommonModule, FormsModule],
-  templateUrl: './produits.component.html',
-  styleUrl: './produits.component.css'
+  selector: 'app-produits', // Sélecteur du composant
+  standalone: true, // Indique que ce composant est autonome
+  imports: [RouterLink, CommonModule, FormsModule], // Modules importés
+  templateUrl: './produits.component.html', // Chemin vers le template HTML
+  styleUrls: ['./produits.component.css'] // Chemin vers les styles CSS
 })
 export class ProduitsComponent implements OnInit {
-  // Tableau pour stocker la liste complète des produits
+  // Tableau pour stocker tous les produits
   maxProduits!: Produit[];
   // Tableau pour stocker les produits de la page actuelle
   produits!: Produit[];
   // Index de la page actuelle, initialisé à 1
   i = 1;
-  // URL de base pour les images
+  // URL de base pour les images des produits
   urlImg = "https://127.0.0.1:8000/images/";
   // Références aux boutons de pagination 'précédent' et 'suivant'
   btnPrecedent!: HTMLElement | null;
   btnSuivant!: HTMLElement | null;
-  // Tableau pour stocker le panier
+  // Tableau pour stocker les articles du panier
   cartItems: any[] = [];
-
-  // Quantité sélectionnée pour gérer le prix dans la modal
+  // Quantité sélectionnée pour le produit
   selectedQuantity: number = 1;
 
-  // Injection du service ProduitsService pour communiquer avec l'API backend
-  constructor(private ProduitsServices: ProduitsService) {}
+  // Injection du service ProduitsService pour interagir avec l'API
+  constructor(private produitsServices: ProduitsService) {}
 
   // Supprimer les balises HTML de la description
   removeDivTags(text: string): string {
-    return text.replace(/<\/?[^>]+(>|$)/g, '').replace(/&nbsp;/g, ' ');
+    return text.replace(/<\/?[^>]+(>|$)/g, '').replace(/&nbsp;/g, ' '); // Remplace les balises par des espaces
   }
 
-  // Fonction pour multiplier le prix en fonction de la quantité sélectionnée
-  changePrice(event: any) {
+  // Met à jour la quantité sélectionnée en fonction de l'événement
+  changePrice(event: Event): void {
     const target = event.target as HTMLSelectElement;
-    if (target !== null) {
-      this.selectedQuantity = Number(target.value);
-    }
+    this.selectedQuantity = Number(target.value); // Convertit la valeur sélectionnée en nombre
   }
 
-  // Fonction pour ajouter un produit au panier
-  addToCart(item: Produit) {
+  // Ajoute un produit au panier
+  addToCart(item: Produit): void {
+    // Crée un objet représentant l'article du panier
     const cartItem = {
       id: item.id,
       quantity: this.selectedQuantity,
       price: item.prix,
+      stock: item.stock,
       imageFileName: item.imageFileName,
       productName: item.productName,
-      commercant: item.userProduct.id
+      commercant: item.userProduct.id // ID du commerçant
     };
 
-    // Récupérer le panier existant depuis le local storage
+    // Récupère le panier existant depuis le local storage
     const cart = JSON.parse(localStorage.getItem('cart') || '[]');
+    const existingItemIndex = cart.findIndex((ci: any) => ci.id === cartItem.id); // Vérifie si l'article existe déjà
 
-    // Vérifier si l'article existe déjà dans le panier et mettre à jour sa quantité
-    const existingItemIndex = cart.findIndex((ci: any) => ci.id === cartItem.id);
     if (existingItemIndex !== -1) {
+      // Si l'article existe, met à jour sa quantité
       cart[existingItemIndex].quantity += cartItem.quantity;
     } else {
-      // Sinon, ajouter le nouvel article au panier
+      // Sinon, ajoute le nouvel article au panier
       cart.push(cartItem);
     }
 
-    // Enregistrer le nouveau panier dans le local storage
+    // Enregistre le panier mis à jour dans le local storage
     localStorage.setItem('cart', JSON.stringify(cart));
-
-    // Mettre à jour les articles du panier
-    this.cartItems = cart;
+    this.cartItems = cart; // Met à jour le tableau des articles du panier
   }
 
-  // Mettre à jour la quantité d'un produit dans le panier
-  updateCartItem(item: Produit, event: any) {
+  // Met à jour la quantité d'un produit dans le panier
+  updateCartItem(item: Produit, event: Event): void {
     const target = event.target as HTMLSelectElement;
     const selectedQuantity = Number(target.value);
 
     const cart = JSON.parse(localStorage.getItem('cart') || '[]');
     const index = cart.findIndex((ci: any) => ci.id === item.id);
+
     if (index !== -1) {
+      // Met à jour la quantité dans le panier
       cart[index].quantity = selectedQuantity;
-      this.cartItems = cart;
-      localStorage.setItem('cart', JSON.stringify(cart));
+      this.cartItems = cart; // Met à jour le tableau des articles du panier
+      localStorage.setItem('cart', JSON.stringify(cart)); // Enregistre le panier mis à jour
     }
   }
 
-  // Fonction pour obtenir le nombre total d'articles dans le panier
+  // Obtient le nombre total d'articles dans le panier
   getTotalItems(): number {
     const cart = JSON.parse(localStorage.getItem('cart') || '[]');
-    return cart.reduce((sum: number, item: any) => sum + item.quantity, 0);
+    return cart.reduce((sum: number, item: any) => sum + item.quantity, 0); // Somme les quantités
   }
 
-  // Fonction pour obtenir le prix total des articles dans le panier
+  // Obtient le prix total des articles dans le panier
   getTotalPrice(): string {
     const totalPrice = this.cartItems.reduce((sum: number, item: any) => {
-      return sum + (item.price * item.quantity);
+      return sum + (item.price * item.quantity); // Calcule le prix total
     }, 0);
-    return totalPrice.toFixed(2) + ' €';
-  }
-  
-  // Fonction pour vider le panier dans le local storage
-  clearCart(): void {
-    // Supprimer le panier dans le local storage
-    localStorage.removeItem('cart');
-    // Réinitialiser le tableau des articles du panier
-    this.cartItems = []
-  }
-    
-  // Fonction pour retirer un produit du panier
-  removeFromCart(item: any): void {
-    // Récupérer le panier existant depuis le local storage
-    const cart = JSON.parse(localStorage.getItem('cart') || '[]');
-    
-    // Trouver l'index de l'article à supprimer
-    const index = cart.findIndex((ci: any) => ci.id === item.id);
-    
-    // Vérifier si l'article existe dans le panier
-    if (index !== -1) {
-      // Si oui, le supprimer du panier
-      cart.splice(index, 1);
-    
-      // Mettre à jour le tableau des articles du panier
-      this.cartItems = cart;
-    
-      // Enregistrer le panier mis à jour dans le local storage
-      localStorage.setItem('cart', JSON.stringify(cart));
-    }
-  }
-  
-  // Appelé après l'initialisation de la vue
-  ngAfterViewInit(): void {
-    // Obtenir des références aux boutons de pagination
-    this.btnPrecedent = document.getElementById("btn-precedent");
-    this.btnSuivant = document.getElementById("btn-suivant");
-    // Mettre à jour la visibilité des boutons en fonction de la page actuelle
-    this.updateButtonVisibility();
+    return totalPrice.toFixed(2) + ' €'; // Retourne le prix formaté
   }
 
-  // Récupérer la page précédente des produits
+  // Vide le panier dans le local storage
+  clearCart(): void {
+    localStorage.removeItem('cart'); // Supprime le panier du local storage
+    this.cartItems = []; // Réinitialise le tableau des articles du panier
+  }
+
+  // Retire un produit du panier
+  removeFromCart(item: any): void {
+    const cart = JSON.parse(localStorage.getItem('cart') || '[]'); // Récupère le panier existant
+    const index = cart.findIndex((ci: any) => ci.id === item.id); // Trouve l'index de l'article à supprimer
+
+    if (index !== -1) {
+      cart.splice(index, 1); // Supprime l'article du panier
+      this.cartItems = cart; // Met à jour le tableau des articles du panier
+      localStorage.setItem('cart', JSON.stringify(cart)); // Enregistre le panier mis à jour
+    }
+  }
+
+  // Appelé après l'initialisation de la vue
+  ngAfterViewInit(): void {
+    this.btnPrecedent = document.getElementById("btn-precedent"); // Récupère le bouton précédent
+    this.btnSuivant = document.getElementById("btn-suivant"); // Récupère le bouton suivant
+    this.updateButtonVisibility(); // Met à jour la visibilité des boutons
+  }
+
+  // Récupère la page précédente des produits
   public getOtherPagesMoins(): void {
-    if (this.i > 1) { // S'assurer de ne pas être à la première page
-      this.i = this.i - 1; // Décrémenter l'index de la page
-      this.ProduitsServices.getOtherPages(this.i).subscribe((data) => {
-        // Mettre à jour les données des produits et rafraîchir la visibilité des boutons
-        this.produits = data;
-        this.updateButtonVisibility();
+    if (this.i > 1) {
+      this.i -= 1; // Décrémente l'index de la page
+      this.produitsServices.getOtherPages(this.i).subscribe((data) => {
+        this.produits = data; // Met à jour les produits de la page actuelle
+        this.updateButtonVisibility(); // Met à jour la visibilité des boutons
       });
     }
   }
-  
-  // Récupérer la page suivante des produits
+
+  // Récupère la page suivante des produits
   public getOtherPagesPlus(): void {
-    this.ProduitsServices.getOtherPages(this.i + 1).subscribe((data) => {
-      if (data && data.length > 0) { // S'assurer que des données existent
-        this.i = this.i + 1; // Incrémenter l'index de la page
-        this.produits = data;
-        this.updateButtonVisibility();
+    this.produitsServices.getOtherPages(this.i + 1).subscribe((data) => {
+      if (data && data.length > 0) {
+        this.i += 1; // Incrémente l'index de la page
+        this.produits = data; // Met à jour les produits de la page actuelle
+        this.updateButtonVisibility(); // Met à jour la visibilité des boutons
       }
     });
   }
-  
+
   // Logique d'initialisation pour le composant, charge les données initiales des produits
   ngOnInit(): void {
-    this.ProduitsServices.getProduits().subscribe((data) => {
-      // Définir les données initiales des produits
-      this.produits = data;
-      
-      // Mettre à jour la visibilité des boutons en fonction des données initiales
-      this.updateButtonVisibility();
+    this.produitsServices.getProduits().subscribe((data) => {
+      this.produits = data; // Définit les produits de la première page
+      this.updateButtonVisibility(); // Met à jour la visibilité des boutons
     });
 
-    // Récupérer et définir les articles du panier depuis le local storage
+    // Récupère et définit les articles du panier depuis le local storage
     const cart = JSON.parse(localStorage.getItem('cart') || '[]');
-    this.cartItems = cart;
+    this.cartItems = cart; // Met à jour le tableau des articles du panier
   }
 
-  // Ajuster la visibilité des boutons de pagination en fonction de la page actuelle
+  // Ajuste la visibilité des boutons de pagination en fonction de la page actuelle
   private updateButtonVisibility(): void {
     if (this.btnPrecedent) {
-      // Masquer le bouton précédent sur la première page
-      this.btnPrecedent!.style.display = this.i === 1 ? "none" : "block";
+      this.btnPrecedent.style.display = this.i === 1 ? "none" : "block"; // Masque le bouton précédent sur la première page
     }
 
     if (this.btnSuivant) {
-      // Déterminer la visibilité du bouton suivant en vérifiant si les données de la page suivante existent
-      this.ProduitsServices.getOtherPages(this.i + 1).subscribe((data) => {
-        this.btnSuivant!.style.display = data && data.length > 0 ? "block" : "none";
+      this.produitsServices.getOtherPages(this.i + 1).subscribe((data) => {
+        this.btnSuivant!.style.display = data && data.length > 0 ? "block" : "none"; // Utilisation de l'opérateur '!'
       });
     }
-  }
+  }    
 }
